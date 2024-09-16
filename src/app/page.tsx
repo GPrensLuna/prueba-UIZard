48460ac579d7afba9c052d27ff59c2b78d7d46ed
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-09-16 18:36:31 -0500

    ok

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "./layout";
+import * as React from "react";
+
+export default function Home(): React.JSX.Element {
+  return (
+    <Layout>
+      <h1>Welcome to the Inlaze Next.js Starter!</h1>
+    </Layout>
+  );
+}
