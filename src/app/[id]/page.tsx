a29975236871549172948b05cce35c70d592a27c
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-09-17 17:08:09 -0500

    ok

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(): JSX.Element {
+  return <h1>Welcome to page!</h1>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,15 @@ export default function RootLayout({
 }>): React.JSX.Element {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body className="grid h-screen grid-rows-[60px,1fr,60px]">
+        <header className="w-full bg-yellow-400 text-black grid place-content-center font-semibold">
+          Uizard HackerNews Reader
+        </header>
+        <main className="grid md:grid-cols-[minmax(250px,380px),1fr] max-w-screen-xl xl:px-40 grid-cols-1">
+          <aside>left bar</aside>
+          <section>{children}</section>
+        </main>
+      </body>
     </html>
   );
 }
